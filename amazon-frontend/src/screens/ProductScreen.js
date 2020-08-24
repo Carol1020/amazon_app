e8309323b0a825247b66eaef3d56060bbf1e7b0e
CarolLiu <carol.gugu@hotmail.com>
2020-08-24 21:43:56 +1000

    product details fetched

--- amazon-frontend/src/screens/ProductScreen.js
+++ amazon-frontend/src/screens/ProductScreen.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import data from '../data';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { detailsProduct } from '../actions/productActions';
 
 function ProductScreen (props) {
-  console.log(props.match.params.id)
-  const product = data.products.find(x=> x._id === props.match.params.id);
+  const [qty, setQty] = useState(1);
+  const productDetails = useSelector(state => state.productDetails);
+  const { product, loading, error } = productDetails;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(detailsProduct(props.match.params.id));
+    return () => {
+
+    }
+  }, []);
+
   return <div>
     <div className="back-to-result">
       <Link to="/">Back to result</Link>
     </div>
+    {loading? <div>Loading</div> :
+    error? <div>{error}</div> :
+    (
     <div className="details">
       <div className="details-image">
         <img src={product.image} alt="product"></img>
@@ -41,7 +55,7 @@ function ProductScreen (props) {
             Status: {product.status}
           </li>
           <li>
-            Qty: <select>
+            Qty: <select value={qty} onChange={(e) => {setQty(e.target.value)}}>
               <option>1</option>
               <option>2</option>
               <option>3</option>
@@ -54,6 +68,8 @@ function ProductScreen (props) {
         </ul>
       </div>
     </div>
+    )}
   </div>
+
 }
 export default ProductScreen
