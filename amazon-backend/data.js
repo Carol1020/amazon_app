ed0d603f76e1d5adfb28c95e9ce2da9b5ad7f2ec
CarolLiu <carol.gugu@hotmail.com>
2020-08-24 18:34:25 +1000

    frontend + backend

---
+++ amazon-backend/data.js
@@ -0,0 +1,44 @@
+export default {
+  products: [
+    {
+      _id:'1',
+      name: 'Slim Shirt',
+      category: 'shirts',
+      image: '/images/d1.jpg',
+      price: 60,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10
+    },
+    {
+      _id:'2',
+      name: 'Best Pants',
+      category: 'pants',
+      image: '/images/d1.jpg',
+      price: 60,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10
+    },
+    {
+      _id:'3',
+      name: 'Slim Shirt',
+      category: 'shirts',
+      image: '/images/d1.jpg',
+      price: 60,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10
+    },
+    {
+      _id:'4',
+      name: 'Fit Pants',
+      category: 'pants',
+      image: '/images/d1.jpg',
+      price: 60,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10
+    }
+  ]
+}
