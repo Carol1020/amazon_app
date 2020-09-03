459b8765915b3e1126046cb76f48047f95942d2b
CarolLiu <carol.gugu@hotmail.com>
2020-09-03 13:40:54 +1000

    use Cookie to store shopping cart info

--- amazon-frontend/src/actions/cartActions.js
+++ amazon-frontend/src/actions/cartActions.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import Cookie from 'js-cookie';
 import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
 
 const addToCart = (productId, qty) => async (dispatch, getState) => {
@@ -16,11 +17,13 @@ const addToCart = (productId, qty) => async (dispatch, getState) => {
         qty
       }
     });
+    const {cart:{cartItems}} = getState();
+    Cookie.set("cartItems", JSON.stringify(cartItems));
   } catch (error) {
 
   }
 }
-const removeFromCart = (productId) => (dispatch) => {
+const removeFromCart = (productId) => (dispatch, getState) => {
   dispatch({type: CART_REMOVE_ITEM, payload: productId})
 }
 export { addToCart, removeFromCart };
