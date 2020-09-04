7eee966d841ebe4e6738a9fd3bd9691f333f2a66
CarolLiu <carol.gugu@hotmail.com>
2020-09-04 11:58:56 +1000

    user authentication frontend

--- amazon-frontend/src/screens/SigninScreen.js
+++ amazon-frontend/src/screens/SigninScreen.js
@@ -1,23 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
+import { signin } from '../actions/userActions';
 
 function SigninScreen (props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const userSignin = useSelector(state => state.userSignin);
+  const { loading, userInfo, error }= userSignin;
   const dispatch = useDispatch();
 
   useEffect(() => {
+    if(userInfo) {
+      props.history.push("/");
+    }
     return () => {}
-  }, []);
+  }, [userInfo]);
   const submitHandler = (e) => {
     e.preventDefault();
+    dispatch(signin(email, password));
   }
   return <div className="form">
     <form onSubmit={submitHandler}>
       <ul className="form-container">
         <li>
           <h2>Sign-In</h2>
+        </li>
+        <li>
+          {loading && <div>Loading...</div>}
+          {error && <div>{error}</div>}
         </li>
         <li>
           <label for="email">
